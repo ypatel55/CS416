19069264fc67cedffb784ec4a96ba861c5c25ade
ypatel55 <ypatel55@illinois.edu>
2022-07-30 18:41:14 -0500

    finishing details

--- project.js
+++ project.js
@@ -1,9 +1,6 @@
 async function loadTwo() {
     const data = await d3.csv('https://ypatel55.github.io/CS416/data/CCSOData.csv');
 
-    var text = "";
-    var opacity = 1, hoverOpacity = 0.50, other = 1, tooltipSize = 15;
-
     const blackData = data.filter(function (d) {
         return d.RACE == 'Black'});
     var blackAVG = d3.sum(blackData.map(function(d) {                
@@ -43,10 +40,7 @@ async function loadTwo() {
 
     const finalData = [{race: "White", value: whiteAVG},{race: "Black", value: blackAVG},{race: "Hispanic", value: hispanicAVG},{race: "Asian/Pacific Islander", value: asianAVG},{race: "Native American", value: nativeAmericanAVG},{race: "White(Hispanic)", value: whiteHispanicAVG}]
 
-    var svg = d3.select("#two"),
-            margin = 200,
-            width = svg.attr("width") - margin,
-            height = svg.attr("height") - margin
+    var svg = d3.select("#two"), margin = 200,width = svg.attr("width") - margin,height = svg.attr("height") - margin
 
     var x = d3.scaleBand().range([0,width]).padding(0.2)
     var y = d3.scaleLinear().range([height,0])
@@ -60,37 +54,22 @@ async function loadTwo() {
     g.append("g").attr("transform", "translate(0," + height + ")").call(d3.axisBottom(x));
     g.append("g").call(d3.axisLeft(y));
 
-
-    let bars = g.selectAll(".bar").data(finalData).enter().append("rect").attr('fill', function(d,i) { return color(i)}).attr("class", "bar").attr("x", function(d) { return x(d.race); }).attr("y", function(d) { return y((d.value)); }).attr("width", x.bandwidth()).attr("height", function(d) { return height - y((d.value)); })
+    g.selectAll(".bar").data(finalData).enter().append("rect").attr('fill', function(d,i) { return color(i)}).attr("class", "bar").attr("x", function(d) { return x(d.race); }).attr("y", function(d) { return y((d.value)); }).attr("width", x.bandwidth()).attr("height", function(d) { return height - y((d.value)); })
     
-    bars.append("text")
-    .text(function(d) { 
-        return finalData[d];
-    })
-    .attr("x", function(d){
-        return x(d) + x.bandwidth()/2;
-    })
-    .attr("y", function(d){
-        return y(finalData[d]) - 5;
-    })
-    .attr("font-family" , "sans-serif")
-    .attr("font-size" , "14px")
-    .attr("fill" , "black")
-    .attr("text-anchor", "middle");
+    svg.append("text").attr("text-anchor", "middle").attr("x", x.bandwidth()/2+120).attr("y", 115).text(Math.round(100*whiteAVG)/100);
+    svg.append("text").attr("text-anchor", "middle").attr("x", x.bandwidth()/2+220).attr("y", 150).text(Math.round(100*blackAVG)/100);
+    svg.append("text").attr("text-anchor", "middle").attr("x", x.bandwidth()/2+318).attr("y", 175).text(Math.round(100*hispanicAVG)/100);
+    svg.append("text").attr("text-anchor", "middle").attr("x", x.bandwidth()/2+412).attr("y", 212).text(Math.round(100*asianAVG)/100);
+    svg.append("text").attr("text-anchor", "middle").attr("x", x.bandwidth()/2+505).attr("y", 95).text(Math.round(100*nativeAmericanAVG)/100);
+    svg.append("text").attr("text-anchor", "middle").attr("x", x.bandwidth()/2+605).attr("y", 245).text(Math.round(100*whiteHispanicAVG)/100);
 
-
-    g.append("g").attr("transform", "translate(0," + height + ")").call(d3.axisBottom(x)).append("text").attr("y", height + 300 ).attr("x", width - 100).attr("text-anchor", "end").attr("stroke", "black").text("Race");
-
-    g.append("g").call(d3.axisLeft(y)).append("text").attr("transform", "rotate(-90)").attr("y", 6).attr("dy", "-5.1em").attr("text-anchor", "end").attr("stroke", "black").text("Average Age at Arrest");
+    g.append("text").attr("text-anchor", "end").attr("x", width/2).attr("y", height + 50).text("Race");
+    g.append("text").attr("text-anchor", "end").attr("transform", "rotate(-90)").attr("y", -50).attr("x", -200).text("Average age at arrest")
 }
-
 
 async function loadThree() {
     const data = await d3.csv('https://ypatel55.github.io/CS416/data/CCSOData.csv');
 
-    var text = "";
-    var opacity = 1, hoverOpacity = 0.50, other = 1, tooltipSize = 15;
-
     const blackData = data.filter(function (d) {
         return d.RACE == 'Black'});
     var blackAVG = d3.sum(blackData.map(function(d) {                
@@ -130,10 +109,7 @@ async function loadThree() {
 
     const finalData = [{race: "White", value: whiteAVG},{race: "Black", value: blackAVG},{race: "Hispanic", value: hispanicAVG},{race: "Asian/Pacific Islander", value: asianAVG},{race: "Native American", value: nativeAmericanAVG},{race: "White(Hispanic)", value: whiteHispanicAVG}]
 
-    var svg = d3.select("#three"),
-            margin = 200,
-            width = svg.attr("width") - margin,
-            height = svg.attr("height") - margin
+    var svg = d3.select("#three"),margin = 200,width = svg.attr("width") - margin,height = svg.attr("height") - margin
 
     var x = d3.scaleBand().range([0,width]).padding(0.2)
     var y = d3.scaleLinear().range([height,0])
@@ -147,21 +123,24 @@ async function loadThree() {
     g.append("g").attr("transform", "translate(0," + height + ")").call(d3.axisBottom(x));
     g.append("g").call(d3.axisLeft(y));
 
-
     g.selectAll(".bar").data(finalData).enter().append("rect").attr('fill', function(d,i) { return color(i)}).attr("class", "bar").attr("x", function(d) { return x(d.race); }).attr("y", function(d) { return y((d.value)); }).attr("width", x.bandwidth()).attr("height", function(d) { return height - y((d.value)); })
+
+    svg.append("text").attr("text-anchor", "middle").attr("x", x.bandwidth()/2+120).attr("y", 410).text(Math.round(100*whiteAVG)/100);
+    svg.append("text").attr("text-anchor", "middle").attr("x", x.bandwidth()/2+220).attr("y", 195).text(Math.round(100*blackAVG)/100);
+    svg.append("text").attr("text-anchor", "middle").attr("x", x.bandwidth()/2+315).attr("y", 425).text(Math.round(100*hispanicAVG)/100);
+    svg.append("text").attr("text-anchor", "middle").attr("x", x.bandwidth()/2+412).attr("y", 575).text(Math.round(100*asianAVG)/100);
+    svg.append("text").attr("text-anchor", "middle").attr("x", x.bandwidth()/2+505).attr("y", 155).text(Math.round(100*nativeAmericanAVG)/100);
+    svg.append("text").attr("text-anchor", "middle").attr("x", x.bandwidth()/2+605).attr("y", 95).text(Math.round(100*whiteHispanicAVG)/100);
     
-    g.append("g").attr("transform", "translate(0," + height + ")").call(d3.axisBottom(x)).append("text").attr("y", height + 300 ).attr("x", width - 100).attr("text-anchor", "end").attr("stroke", "black").text("Race");
-
-    g.append("g").call(d3.axisLeft(y)).append("text").attr("transform", "rotate(-90)").attr("y", 6).attr("dy", "-5.1em").attr("text-anchor", "end").attr("stroke", "black").text("Average Days in Jail");
+    g.append("text").attr("text-anchor", "end").attr("x", width/2).attr("y", height + 50).text("Race");
+    g.append("text").attr("text-anchor", "end").attr("transform", "rotate(-90)").attr("y", -50).attr("x", -200).text("Average number of days in jail")
 }
-
-
 
 async function loadFour() {
     const csvData = await d3.csv('https://ypatel55.github.io/CS416/data/CCSOData.csv');
+    //data created with count of each race's bookings amount
     var data = [{race: "White", value: 25175},{race: "Black", value: 36319},{race: "Hispanic", value: 4740},{race: "Asian/Pacific Islander", value: 751},{race: "Native American", value: 54},{race: "White(Hispanic)", value: 20}]
 
-    var text = "";
     var width = 800, height = 800, opacity = 1, hoverOpacity = 0.50, other = 1, tooltipSize = 15, piechart = "#four";
 
     var radius = Math.min(width, height) / 2;
@@ -190,8 +169,6 @@ async function loadFour() {
         .attr("class", "name-text")
         .text(`${d.data.race} (${d.data.value}) (${(Math.round(1000 * d.data.value / 67059) / 10) + '%'})`)
         .attr('text-anchor', 'middle');
-
-        let text = g.select("text");
       })
     .on("mousemove", function(d) {
           let mousePosition = d3.mouse(this);
@@ -234,15 +211,12 @@ let keys = legend.selectAll('.key')
       .text(d => `${d.race}`);
 
     keys.exit().remove();
-     
 }
 
 async function loadFive() {
     const data = await d3.csv('https://ypatel55.github.io/CS416/data/CCSOData.csv');
 
     const races = ["White", "Black", "Hispanic", "Asian/Pacific Islander","Native American","White (Hispanic)"]
-
-    var colorCount = 0;
 
     d3.select("#select-race")
             .selectAll('options')
@@ -256,71 +230,31 @@ async function loadFive() {
                 return d;
             })
 
-    const blackData = data.filter(function (d) {
-        return d.RACE == 'Black'});
-
-    const whiteData = data.filter(function (d) {
-        return d.RACE == 'White'});
-
-    const hispanicData = data.filter(function (d) {
-        return d.RACE == 'Hispanic'});
-
-    const asianData = data.filter(function (d) {
-        return d.RACE == 'Asian/Pacific Islander'});
-
-
-    const nativeAmericanData = data.filter(function (d) {
-        return d.RACE == 'Native American'});
-
-
-    const whiteHispanicData = data.filter(function (d) {
-        return d.RACE == 'White (Hispanic)'});
-
-    var color = d3.scaleOrdinal(['#85E3FF','#FFB5E8','#AFF8DB','#B5B9FF','#FFCBC1', '#fff06e']);
-
     var margin = {top: 10, right: 30, bottom: 30, left: 60},
     width = 800 - margin.left - margin.right,
     height = 800 - margin.top - margin.bottom;
 
+    var svg = d3.select("#five").append("svg").attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom).append("g").attr("transform","translate(" + margin.left + "," + margin.top + ")");
 
-    var svg = d3.select("#five")
-     .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
-
-    var x = d3.scaleLinear()
-    .domain([0, d3.max(data, function(d) { return d.AgeatArrest; })])
-          .range([ 0, width ]);
-        svg.append("g")
-          .attr("transform", "translate(0," + height + ")")
-          .call(d3.axisBottom(x).ticks(7));
+    var x = d3.scaleLinear().domain([0, d3.max(data, function(d) { return d.AgeatArrest; })]).range([ 0, width ]);
+    svg.append("g").attr("transform", "translate(0," + height + ")").call(d3.axisBottom(x).ticks(7));
     
-        var y = d3.scaleLinear()
-        .domain([0, d3.max(data, function(d) { return d.DaysinJail; })])
-          .range([ height, 0 ]);
-        svg.append("g")
-          .call(d3.axisLeft(y));
+    var y = d3.scaleLinear().domain([0, d3.max(data, function(d) { return d.DaysinJail; })]).range([ height, 0 ]);
+    svg.append("g").call(d3.axisLeft(y));
 
-      var dots = svg.selectAll("circle").data(data.filter(function(d){return d.RACE=="White"})).enter().append("circle").attr("cx", function(d) { return x(d.AgeatArrest)}).attr("cy", 
-      function(d) { return y(d.DaysinJail)}).attr("r", 3)
+    svg.selectAll("circle").data(data.filter(function(d){return d.RACE=="White"})).enter().append("circle").attr("cx", function(d) { return x(d.AgeatArrest)}).attr("cy", function(d) { return y(d.DaysinJail)}).attr("r", 3).attr("fill", "navy")
 
-
-      function update(selectedGroup) {
-    
+    function update(selectedGroup) {
         var dataFilter = data.filter(function(d){return d.RACE==selectedGroup})
-
-      svg.selectAll("circle").remove()
-      svg.selectAll("circle").data(dataFilter).enter().append("circle").attr("cx", function(d) { return x(d.AgeatArrest)}).attr("cy", 
-      function(d) { return y(d.DaysinJail)}).attr("r", 3)
-      }
+        svg.selectAll("circle").remove()
+        svg.selectAll("circle").data(dataFilter).enter().append("circle").attr("cx", function(d) { return x(d.AgeatArrest)}).attr("cy", function(d) { return y(d.DaysinJail)}).attr("r", 3).attr("fill", "navy")
+     }
   
-      d3.select("#select-race").on("change", function(d) {
-          var selectedOption = d3.select(this).property("value")
-          update(selectedOption)
-      })
+    d3.select("#select-race").on("change", function(d) {
+        var selectedOption = d3.select(this).property("value")
+        update(selectedOption)
+    })    
 
-
+    svg.append("text").attr("text-anchor", "end").attr("x", width/2).attr("y", height + margin.top + 18).text("Age at arrest");
+    svg.append("text").attr("text-anchor", "end").attr("transform", "rotate(-90)").attr("y", -margin.left+20).attr("x", -250).text("Number of days in jail")
 }
